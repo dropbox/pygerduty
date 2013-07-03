34d575daa8ebc3b4dba2acbbe7b43f1fade82c48
Leon Berov <leon@depop.com>
2013-07-03 11:59:05 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 kwargs = {
     "name": "pygerduty",
-    "version": "0.12",
+    "version": "0.13",
     "py_modules": ["pygerduty"],
     "scripts": ["bin/grab_oncall.py"],
     "description": "Python Client Library for PagerDuty's REST API",
