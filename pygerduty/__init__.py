fb7a165aaac55400121e64cd9dd6c9c054705843
Chris Martin <ch.martin@gmail.com>
2015-09-08 23:23:06 -0700

    Fix relative import

--- pygerduty/__init__.py
+++ pygerduty/__init__.py
@@ -13,7 +13,7 @@ except ImportError:
 
 
 __author__ = "Gary M. Josack <gary@dropbox.com>"
-from version import __version__, version_info  # noqa
+from .version import __version__, version_info  # noqa
 
 
 # TODO:
